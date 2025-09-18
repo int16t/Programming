32caefaf8b7d151466979b4e49e8f5e41c2ffbed
int16t <gy4ra@protonmail.com>
2025-09-18 10:30:53 -0300

    feat: exec timing for command exercise

---
+++ Level_2/exectiming.c
@@ -0,0 +1,40 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/time.h>
+#include <unistd.h>
+#include <sys/wait.h>
+
+struct timeval start, end;
+
+int main(int argc, char *argv[]){
+
+    pid_t pid;
+
+    gettimeofday(&start, NULL);
+
+    pid = fork();
+
+    if (pid == 0) {
+        execvp(argv[1], &argv[1]);
+        perror("execvp falhou");
+        exit(1);
+    } else if (pid > 0) {
+        // processo pai: espera o filho terminar
+        int status;
+        waitpid(pid, &status, 0);
+
+        // pega o tempo final
+        gettimeofday(&end, NULL);
+
+        // calcula diferença em segundos e microssegundos
+        double elapsed = (end.tv_sec - start.tv_sec) +
+                         (end.tv_usec - start.tv_usec) / 1000000.0;
+
+        printf("Tempo de execução: %.6f segundos\n", elapsed);
+    } else {
+        perror("fork falhou");
+        return 1;
+    }
+
+    return 0;
+}
